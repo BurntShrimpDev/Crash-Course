644e763c01ee13195b3269922e7336a3bbb16ccf
Callum <Brogancsp@gmail.com>
2025-10-21 20:03:03 +0200

    Character Classes

---
+++ Source/CrashCourse/Private/Characters/CC_BaseCharacter.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CrashCourse/Public/Characters/CC_BaseCharacter.h"
+
+
+ACC_BaseCharacter::ACC_BaseCharacter()
+{
+	
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Ensure animations and bone transforms update even when the mesh isn't visible (needed for accurate bone data in gameplay logic)
+	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+}
+
+
+
+
+
