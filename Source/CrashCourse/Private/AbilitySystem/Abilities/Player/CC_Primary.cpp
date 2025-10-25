066e76d451d7075f5bd36b9ff52b285559288222
Callum <Brogancsp@gmail.com>
2025-10-25 21:37:46 +0200

    Hit React Gameplay Event

--- Source/CrashCourse/Private/AbilitySystem/Abilities/Player/CC_Primary.cpp
+++ Source/CrashCourse/Private/AbilitySystem/Abilities/Player/CC_Primary.cpp
@@ -3,9 +3,11 @@
 
 #include "AbilitySystem/Abilities/Player/CC_Primary.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Engine/OverlapResult.h"
+#include "GameplayTags/CCTags.h"
 
-void UCC_Primary::HitBoxOverlapTest()
+TArray<AActor*> UCC_Primary::HitBoxOverlapTest()
 {
 	TArray<AActor*> ActorsToIgnore;
 	ActorsToIgnore.Add(GetAvatarActorFromActorInfo());
@@ -23,19 +25,46 @@ void UCC_Primary::HitBoxOverlapTest()
 
 	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector() * HitBoxForwardOffset;
 	const FVector HitBoxLocation = GetAvatarActorFromActorInfo()->GetActorLocation() + Forward + FVector(0, 0, HitBoxElevationOffset);
+	
 	GetWorld()->OverlapMultiByChannel(OverlapResults,HitBoxLocation, FQuat::Identity, ECC_Visibility, Sphere, QueryParams, ResponseParams);
+
+	TArray<AActor*> ActorsHit;
+	for (const FOverlapResult& Result : OverlapResults)
+	{
+		if (!IsValid(Result.GetActor())) continue;
+		ActorsHit.AddUnique(Result.GetActor());
+	}
 
 	if (bDrawDebugs)
 	{
-		DrawDebugSphere(GetWorld(), HitBoxLocation, HitBoxRadius, 16, FColor::Red, false, 3.0f);
-		for (const FOverlapResult& Result : OverlapResults)
+		DrawHitBoxOverlapDebugs(OverlapResults, HitBoxLocation);
+	}
+
+	return ActorsHit;
+}
+
+void UCC_Primary::SendHitReactEventToActors(const TArray<AActor*>& ActorsHit)
+{
+	
+	for (AActor* HitActor : ActorsHit)
+	{
+		FGameplayEventData Payload;
+		Payload.Instigator = GetAvatarActorFromActorInfo();
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(HitActor, CCTags::Events::Enemy::HitReact, Payload);
+	}
+	
+}
+
+void UCC_Primary::DrawHitBoxOverlapDebugs(const TArray<FOverlapResult>& OverlapResults, const FVector& HitBoxLocation) const 
+{
+	DrawDebugSphere(GetWorld(), HitBoxLocation, HitBoxRadius, 16, FColor::Red, false, 3.0f);
+	for (const FOverlapResult& Result : OverlapResults)
+	{
+		if (IsValid(Result.GetActor()))
 		{
-			if (IsValid(Result.GetActor()))
-			{
-				FVector DebugLocation = Result.GetActor()->GetActorLocation();
-				DebugLocation.Z += 100.f;
-				DrawDebugSphere(GetWorld(), DebugLocation, 30.f, 10, FColor::Green, false, 3.0f);
-			}
+			FVector DebugLocation = Result.GetActor()->GetActorLocation();
+			DebugLocation.Z += 100.f;
+			DrawDebugSphere(GetWorld(), DebugLocation, 30.f, 10, FColor::Green, false, 3.0f);
 		}
 	}
 }
