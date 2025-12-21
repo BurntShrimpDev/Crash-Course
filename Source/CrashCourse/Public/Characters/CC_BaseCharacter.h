d093b13cbfa909190c52335a37eb0f152d08b996
Callum <Brogancsp@gmail.com>
2025-12-21 03:41:42 +0100

    Search Range

--- Source/CrashCourse/Public/Characters/CC_BaseCharacter.h
+++ Source/CrashCourse/Public/Characters/CC_BaseCharacter.h
@@ -45,6 +45,9 @@ public:
 	UFUNCTION(BlueprintImplementableEvent)
 	void RotateToTarget(AActor* RotateTarget);
 
+	UPROPERTY(EditAnywhere, Category="Crash|AI")
+	float SearchRange{1000.f};
+
 protected:
 	void GiveStartupAbilities();
 
